f614ba8614116835d9a69ab07a26fe4ed47284ba
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-24 00:14:51 +0300

    fix main

--- mainv3.sh
+++ mainv3.sh
@@ -1,19 +1,31 @@
 #! /bin/bash
 
-DEBIAN_FRONTEND=noninteractive
+set -e
+
+VERSION="0.11.3.ferreogit"
+
+export DEBIAN_FRONTEND="noninteractive"
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_BUILD_OPTIONS="nocheck notest terse"
+export DPKG_GENSYMBOLS_CHECK_LEVEL=0
 
 # Clone Upstream
 git clone https://github.com/ferrreo/booster.git
-cd booster
-cp -rvf ../debian ./
-cp -f ../Makefile ./Makefile
-cp -f ../booster.yml ./booster.yml
-cp -f ../update-initramfs ./update-initramfs
+cp -rvf ./debian ./booster/
+cp -vf ./Makefile ./booster/
+cp -vf ./booster.yml ./booster/
+cp -vf ./update-initramfs ./booster/
+cd ./booster
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
+LOGNAME=root dh_make --createorig -y -l -p booster_"$VERSION" || echo "dh-make: Ignoring Last Error"
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
